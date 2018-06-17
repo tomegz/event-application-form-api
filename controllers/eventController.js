3a7bf17d4fe5e1f292aa4f0e6b41d0c15cf01993
tomegz <tomaszbubala@o2.pl>
2018-06-17 11:09:50 +0200

    change api response

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -3,5 +3,5 @@ const Event = mongoose.model('Event');
 
 exports.createEvent = async (req, res) => {
   const event = await (new Event(req.body)).save();
-  res.json({ text: "Succesfully created event", type: "success"});
+  res.json([{ text: "Succesfully created event", type: "success"}]);
 };
